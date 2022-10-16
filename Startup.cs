b194d50a9ef5d1d1b175e497214def68cc4f434c
Welliton Fernandes Leal <wellfernandes@me.com>
2022-10-16 16:54:27 -0300

    project migrated from .NET Core 2.1 version to .NET Core 3.1

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
